e523feee164d4988c9d2db26719d0aaf2f30f214
belindamapedza <belindamapedza02@yahoo.com>
2019-02-28 11:54:22 +0200

    functions
    i have been working on maths

---
+++ maths.js
@@ -0,0 +1,7 @@
+var x,y;
+x=5;
+y=6;
+document.getElementById("demo").innerHTML=x+y;
+document.getElementById("one").innerHTML=x-y;
+document.getElementById("two").innerHTML=x*y;
+document.getElementById("three").innerHTML=x/y;
