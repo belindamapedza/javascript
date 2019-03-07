c1063f2154af751c4621fe9e09de68b634ffee11
belindamapedza <belindamapedza02@yahoo.com>
2019-03-07 09:05:30 +0200

    if conditional statement
    we used if condition to display tuesday and wednesday in our loops

--- if.js
+++ if.js
@@ -1,7 +1,19 @@
-var days = ["Monday","Tuesday","Wednesday"]; 
+var days = ["Monday","Tuesday","Wednesday"];
 
 if (days[0] == "Monday"){
-    document.write("Hey I found Monday");
+    document.write("Hey I found Monday <br> ");
   } else {
       document.write("Bummer man, no Monday here");
+  }
+
+  if (days[1] == "Tuesday"){
+      document.write("Hey I found Tuesday <br> ");
+  }else{
+      document.write("Bummer man, no Tuesday here");
+  }
+
+  if (days[2] == "Wednesday"){
+    document.write("Hey I found Wednesday <br>");
+  }else{
+    document.write("Bummer man, no Wednesday here");
   }
