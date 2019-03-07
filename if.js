c13422bdbe8627077ebad7170b05de23f9371a3b
belindamapedza <belindamapedza02@yahoo.com>
2019-03-07 08:43:00 +0200

    if condition
    we used if condition to display our elements

---
+++ if.js
@@ -0,0 +1,7 @@
+var days = ["Monday","Tuesday","Wednesday"]; 
+
+if (days[0] == "Monday"){
+    document.write("Hey I found Monday");
+  } else {
+      document.write("Bummer man, no Monday here");
+  }
